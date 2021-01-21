a19217cfcb58c4ea2d8afd706d0210bc85994327
simit patel <simitp@gmail.com>
2021-01-21 09:32:24 -0500

    order sort bug fix correction

--- R/get-polygon-prices-hourly.R
+++ R/get-polygon-prices-hourly.R
@@ -9,31 +9,16 @@ get_polygon_hourly_prices <- function(ticker,from = Sys.Date()-61, to = Sys.Date
       to <- as.Date(to)
     }
   }
-  date_set <- seq.Date(from = from, to = to, by = 9)
-  date_partner <- rep(1:(length(date_set)/2+1), each = 2,length.out = length(date_set))
-  date_table <- date_set %>% tibble::as_tibble() %>% dplyr::mutate(pair = date_partner)
-  if(nrow(date_table) %% 2 > 0) {
-    date_table <- dplyr::bind_rows(date_table,tibble::tibble(value = to,pair = max(date_partner)))
+  polygon_list <- glue::glue("https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/hour/{from}/{to}?sort=asc&apiKey={Sys.getenv('polygon')}&limit=50000") %>%
+    httr::GET() %>% httr::content()
+  if(class(polygon_list) != 'list' | length(polygon_list$results) == 0) {
+    return(tibble::tibble())
   }
-  poly_inner <- function(date_pair) {
-    date_values <- date_table %>% dplyr::filter(pair == date_pair)
-    polygon_list <- glue::glue("https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/hour/{date_values$value[1]}/{date_values$value[2]}?sort=desc&apiKey={Sys.getenv('polygon')}") %>%
-      httr::GET() %>% httr::content()
-    if(class(polygon_list) != 'list' | length(polygon_list$results) == 0) {
-      return(tibble::tibble())
-    }
-    polygon_df <- polygon_list %>% purrr::pluck('results') %>% dplyr::bind_rows() %>%
-      dplyr::mutate(t = t/1000, t = as.POSIXct(t, origin = '1970-01-01'))
-    return(polygon_df)
-  }
-  return_set <- purrr::map_dfr(1:(max(date_partner)),poly_inner)
-  if(nrow(return_set) == 0) {
-    return(return_set)
-  }
-  if(nrow(return_set) > 0) {
-    return_set <- return_set %>% dplyr::mutate(ticker = ticker) %>%
-      dplyr::rename (date = t, open = o, high = h, low = l, close = c, volume = v,
-                     volume_weighted_avg_price = vw, trade_count = n)
-  }
-  return(return_set)
+  polygon_df <- polygon_list %>% purrr::pluck('results') %>% dplyr::bind_rows() %>%
+    dplyr::mutate(t = t/1000, t = as.POSIXct(t, origin = '1970-01-01')) %>%
+    dplyr::rename (date = t, open = o, high = h, low = l, close = c, volume = v,
+                   volume_weighted_avg_price = vw) %>% select(-n) %>%
+    dplyr::mutate(ticker = ticker) %>%
+    dplyr::select(c(ticker,date,open,high,low,close,volume_weighted_avg_price,volume))
+  return(polygon_df)
 }
