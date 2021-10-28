7b49bb13505fc1755ddbaec2b360aba734ccfc35
simit patel <simitp@gmail.com>
2021-10-27 22:18:32 -0400

    up

--- R/get-polygon-price-today.R
+++ R/get-polygon-price-today.R
@@ -3,7 +3,7 @@ get_polygon_price_today <- function(symbol) {
   polygon_list <- glue::glue("https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers/{symbol}?apiKey={Sys.getenv('polygon')}") %>%
     httr::GET() %>% httr::content()
   if(polygon_list$status == 'NotFound') {
-    return('ticker not found. please confirm it is a real, currently listed, US ticker.')
+    return(tibble())
   }
   polygon_df <- polygon_list %>% purrr::pluck('ticker') %>% purrr::pluck('day') %>% dplyr::bind_rows() %>%
     rename(close = c, high = h, low = l, open = o,volume = v, volume_weighted_avg_price = vw) %>%
